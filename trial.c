76ba543ce1d907255650d70a4605a9d539b65501
CrimeWizard <youssefhamed4002@gmail.com>
2025-03-09 01:04:15 +0200

    set single core, set sched policy, added thread attributes, added time funciton

--- trial.c
+++ trial.c
@@ -1,6 +1,21 @@
-// Online C compiler to run C program online
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
+#include <sched.h>
+#include <time.h>
+#include <unistd.h>
+
+#define POLICY SCHED_FIFO 
+
+double get_time_ms()
+{
+ struct timespec ts;
+ clock_gettime(CLOCK_MONOTONIC, &ts);
+ return ts.tv_sec * 1000.0 + ts.tv_nsec / 1.0e6; // Convert to milliseconds
+}
+
+
 
 void* func1(void* arg){
     printf("Inside the first thread\n");
@@ -78,22 +93,41 @@ void* func3(void* arg){
 
 int main() {
     pthread_t ptid1, ptid2, ptid3;
+
+    struct sched_param param; 
+
+    // Define CPU affinity set
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset); // Initialize the CPU set
+    CPU_SET(0, &cpuset); // Assign execution to CPU 0
+
+    pthread_attr_t attr; // Thread attributes
+    pthread_attr_init(&attr); // Initialize thread attributes
+    pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpuset); // Set thread CPU affinity
+
+    pthread_attr_setschedpolicy(&attr, POLICY); //setting schedule policy
+
+    if (POLICY == SCHED_FIFO || POLICY == SCHED_RR) {
+        param.sched_priority = sched_get_priority_max(POLICY); // Highest priority
+        pthread_attr_setschedparam(&attr, &param);
+        pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
+    }
     
     //Thread 1 
-    pthread_create(&ptid1, NULL, &func1, NULL);
+    pthread_create(&ptid1, &attr, &func1, NULL); 
     pthread_join(ptid1, NULL);
     
     // Thread 2
-    pthread_create(&ptid2, NULL, &func2, NULL);
+    pthread_create(&ptid2, &attr, &func2, NULL);
     pthread_join(ptid2, NULL);
     
     //Thread 3
-    pthread_create(&ptid3, NULL, &func3, NULL);
+    pthread_create(&ptid3, &attr, &func3, NULL);
     pthread_join(ptid3, NULL);
     
+    pthread_attr_destroy(&attr); 
+
     printf("Threads Ended \n");
-    
-    pthread_exit(NULL);
     
     return 0;
 }
